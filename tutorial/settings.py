06a65809217a528c40447a7e1905a92dd5b903d8
partysh <partysh@aliyun.com>
2019-01-09 22:20:34 +0800

    fix error [CSRFCheck object has no attribute process_request]

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
